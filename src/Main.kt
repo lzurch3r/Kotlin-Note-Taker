9918025c6de44233e675fd91978ca8960b9e65d1
Lev Zurcher <lazurcher5@gmail.com>
2026-05-15 18:12:43 -0600

    File saving and reading

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,16 @@
+import java.io.FileInputStream
 import java.io.Serializable
 import java.io.ObjectOutputStream
 import java.io.FileOutputStream
+import java.io.ObjectInputStream
 
 fun main(args: Array<String>) {
     //TEST NOTE, Save to file
     val newNote = Note("New Note", "Description here", "No category")
 
     save_to_file(newNote, "notes.dat")
+    val notes = load_from_file("notes.dat")
+    notes.display()
 }
 
 fun load_home_page() {
@@ -34,12 +38,22 @@ fun get_category() {
 
 }
 
-data class Note(val title: String, val description: String, val category: String) {
+data class Note(val title: String, val description: String, val category: String): Serializable {
     private var date = ""
+
+    public fun display() {
+        println("$title - $description - $date")
+    }
 }
 
 fun save_to_file(note: Note, path: String) {
     ObjectOutputStream(FileOutputStream(path)).use { out ->
         out.writeObject(note)
+    }
+}
+
+fun load_from_file(path: String): Note {
+    return ObjectInputStream(FileInputStream(path)).use { input ->
+        input.readObject() as Note
     }
 }
