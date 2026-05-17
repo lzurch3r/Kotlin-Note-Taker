7ee5abc181ccb6a84b8e6e9bef72fe02ed7c42e4
Lev Zurcher <lazurcher5@gmail.com>
2026-05-16 21:30:28 -0600

    Update main.kt
    Loads main menu and prompts user input. Error checks in place for invalid string-to-byte conversion.

--- src/Main.kt
+++ src/Main.kt
@@ -4,6 +4,7 @@ import java.io.ObjectOutputStream
 import java.io.FileOutputStream
 import java.io.ObjectInputStream
 import java.time.LocalDateTime
+import java.io.File
 
 fun main(args: Array<String>) {
 
@@ -23,13 +24,44 @@ fun main(args: Array<String>) {
     println(string)*/
 
     //TEST create_note()
-    val newNote = create_note()
-    newNote.display()
+    /*val newNote = create_note()
+    newNote.display()*/
+
+    //TEST reading and writing to "notes.dat"
+    load_home_page()
+
+    var option = "";
+    option = get_user_input("Type a number and press Enter: ")
+    while (option.length > 1) {
+        println("Invalid input. Please enter a number")
+        option = get_user_input("")
+    }
+    when (option.toByteOrNull()) {
+        1.toByte() -> create_note()
+        2.toByte(), 3.toByte(), 4.toByte() -> println("Not implemented yet")
+        else -> println("Invalid option")
+    }
 }
 
 fun load_home_page() {
-    println("Welcome! Here are your notes:")
-    val notes = ""
+    val path = "notes.dat"
+    val notesFile = File(path)
+    var notes: MutableCollection<Note> = mutableSetOf()
+
+    if (!notesFile.exists()) {
+        println("You have no notes!")
+    }
+
+    else if (notesFile.exists()) {
+        notes = load_from_file("notes.dat")
+        display_set_of_notes(notes)
+    }
+
+    println("MAIN MENU")
+    println("1. Create new note")
+    println("2. Delete existing note")
+    println("3. Add existing note to category")
+    println("4. Sort notes")
 }
 
 fun get_user_input(prompt: String): String {
@@ -89,8 +121,8 @@ fun save_to_file(notes: Collection<Note>, path: String) {
     }
 }
 
-fun load_from_file(path: String): Collection<Note> {
+fun load_from_file(path: String): MutableCollection<Note> {
     return ObjectInputStream(FileInputStream(path)).use { input ->
-        input.readObject() as Collection<Note>
+        input.readObject() as MutableCollection<Note>
     }
 }
