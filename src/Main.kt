db683fff765a1d53206468128eba12dfae52f96d
Lev Zurcher <lazurcher5@gmail.com>
2026-05-23 14:22:53 -0600

    Update Main.kt
    Implemented function to assign a note to a category.
    Also, fixed note deletion (notes were visible to the user sorted in descending order, but the notes saved to notes.dat were saved in ascending order, meaning that the index of notes were inverted, deleting the wrong one).

--- src/Main.kt
+++ src/Main.kt
@@ -5,6 +5,7 @@ import java.io.FileOutputStream
 import java.io.ObjectInputStream
 import java.time.LocalDateTime
 import java.io.File
+import java.time.format.DateTimeFormatter
 
 fun main(args: Array<String>) {
     //If there are notes, loads display of note collection and menu options
@@ -20,10 +21,8 @@ fun main(args: Array<String>) {
     when (option.toByteOrNull()) {
         1.toByte() -> { loadCreateNotePage(notes, sortType) }
         2.toByte() if (notes.isNotEmpty()) -> { loadDeleteNotePage(notes, sortType) }
-        3.toByte() -> println("Not implemented yet")
-        4.toByte() -> {
-            sortType = loadSortNotePage(notes)
-        }
+        3.toByte() -> { loadAssignCategoryPage(notes) }
+        4.toByte() -> { sortType = loadSortNotePage(notes) }
         5.toByte() -> {
             println("Exiting note-taking program...")
             println("See you!")
@@ -51,7 +50,7 @@ fun loadHomePage(): MutableCollection<Note> {
     println("1. Create new note")
     if (notes.isNotEmpty()) {
         println("2. Delete existing note")
-        println("3. Link category to existing note")
+        println("3. Assign existing note to category")
         println("4. Sort notes")
     }
     println("5. Exit program")
@@ -68,23 +67,22 @@ fun loadCreateNotePage(notes: MutableCollection<Note>, sortType: Int) {
     val newNote = createNote()
     notes.add(newNote)
 
-    saveToFile(notes.sortedBy { it.date_time }, "notes.dat")
+    saveToFile(notes.sortedByDescending { it.dateTime }, "notes.dat")
     println("Note saved!")
     displaySetOfNotes(notes, sortType)
 }
 
 fun loadDeleteNotePage(notes: MutableCollection<Note>, sortType: Int) {
     var noteNum = getUserInput("Type the number of the note you want to delete: ")
-    println("You typed: $noteNum")
+
     while (noteNum.toInt() > notes.size || noteNum.toInt() <= 0) {
-        print("Note doesn't exist. Please enter a number within range: ")
-        noteNum = getUserInput("")
+        noteNum = getUserInput("Note doesn't exist. Please enter a number within range: ")
     }
     println("Deleting note $noteNum...")
 
     notes.remove(notes.elementAt(noteNum.toInt() - 1))
 
-    saveToFile(notes, "notes.dat")
+    saveToFile(notes.sortedByDescending { it.dateTime }, "notes.dat")
     println("Note deleted!")
     displaySetOfNotes(notes, sortType)
 }
@@ -110,6 +108,33 @@ fun loadSortNotePage(notes: MutableCollection<Note>): Int {
     return sortType
 }
 
+fun loadAssignCategoryPage(notes: MutableCollection<Note>) {
+    var localNotes = notes
+    val sortType = 1
+
+    //Prompt user to change a note
+    var noteNum = getUserInput("Type the number of the note you want to assign: ")
+    while (noteNum.toInt() > localNotes.size || noteNum.toInt() <= 0) {
+        noteNum = getUserInput("Note doesn't exist. Please enter a number within range: ")
+    }
+
+    //Get note and prompt user to change to a category
+    var note = localNotes.elementAt(noteNum.toInt() - 1)
+    note.display()
+
+    var newCategory = getUserInput("Type in a new category (or leave blank to assign it to none): ")
+
+    //Change category and display notes in sorted order
+    note.category = newCategory
+
+    //Save to "notes.dat"
+    localNotes.remove(localNotes.elementAt(noteNum.toInt() - 1))
+    localNotes.add(note)
+    saveToFile(localNotes.sortedByDescending { it.dateTime }, "notes.dat")
+    println("Note saved!\n")
+    displaySetOfNotes(localNotes, sortType)
+}
+
 fun createNote(): Note {
     println("Let's create a note!")
     var title: String = getUserInput("Title: ")
@@ -123,17 +148,17 @@ fun createNote(): Note {
         description = getUserInput("Description: ")
     }
     //category is optional
-    var category = getUserInput("Category (optional): ")
+    val category = getUserInput("Category (optional): ")
 
     return Note(title, description, category, LocalDateTime.now())
 }
 
 fun displaySetOfNotes(notes: Collection<Note>, sortType: Int) {
     val localNotes = when (sortType) {
-        1 -> notes.sortedByDescending { it.date_time }
+        1 -> notes.sortedByDescending { it.dateTime }
         2 -> notes.sortedBy { it.title }
         3 -> notes.sortedBy { it.category }
-        else -> notes.sortedByDescending { it.date_time }
+        else -> notes.sortedByDescending { it.dateTime }
     }
     var count = 1
     for (note in localNotes) {
@@ -142,18 +167,22 @@ fun displaySetOfNotes(notes: Collection<Note>, sortType: Int) {
         println("")
         count++
     }
-
 }
 
-data class Note(val title: String, val description: String, var category: String, val date_time: LocalDateTime): Serializable {
+data class Note(val title: String, val description: String, var category: String, val dateTime: LocalDateTime): Serializable {
     fun display() {
+        //Format Date and Time to be more readable
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+        val formatted = dateTime.format(formatter)
+
+        //Display all components of Note object
         println("Title: $title")
         println("   Description: $description")
         if (category.isEmpty()) {
             println("   Category: <none>")
         }
         else println("   Category: $category")
-        println("   Date and Time: $date_time")
+        println("   Date and Time: $formatted")
     }
 }
 
@@ -165,7 +194,8 @@ fun saveToFile(notes: Collection<Note>, path: String) {
 
 fun loadFromFile(path: String): MutableCollection<Note> {
     return ObjectInputStream(FileInputStream(path)).use { input ->
-        val loaded = input.readObject() as Collection<Note>
+        @Suppress("UNCHECKED_CAST")
+        val loaded = input.readObject() as? Collection<Note> ?: emptyList()
         mutableListOf<Note>().apply { addAll(loaded) }
     }
 }
